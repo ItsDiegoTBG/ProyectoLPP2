c8d6943b20bee8ab5ed8e8d31e0f83ede6f2dc38
ItsDiegoTBG <diealcon@espol.edu.ec>
2024-08-25 19:15:33 -0500

    Correciones Front.

--- shop.php
+++ shop.php
@@ -31,7 +31,7 @@
 
 <div class="container" id="menu-navegacion">
 	<img src="images/05b797331b8e29dea04657dd7fc7d3c86554cd8fr1-630-630v2_hq-removebg-preview.png" class="img-fluid" id="foto-logo">
-	<a class="navbar-brand" href="index.html">Silverhand Components<span>.</span></a>
+	<a class="navbar-brand" href="index.php">Silverhand Components<span>.</span></a>
 
 	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
@@ -172,24 +172,8 @@
 		    </div>
 
 	<div class="review-section">
-    <h2>Submit a Review</h2>
-    <form action="back-end/submit_review.php" method="post">
-        <input type="hidden" name="product_id" value="product_id_here">
-        <label for="rating">Rating:</label>
-        <input type="number" name="rating" min="1" max="5" required>
-        <label for="review">Review:</label>
-        <textarea name="review" required></textarea>
-        <button type="submit">Submit Review</button>
-    </form>
-</div>
 
-<div id="reviews-container">
-    <?php
-   # $productId = 'product_id_here'; 
-    include 'back-end/get_reviews.php';
-    ?>
-</div> 
-
+	<div class="container">
 <div class="subscription-form">
 						<h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Comenta Aqui sobre nuestros productos actuales!</span></h3>
 
@@ -222,131 +206,122 @@
 							</div>
 						</form>
 					</div>
-
-						<div id="resultados" class="blog-section">
-				<div class="container">
-					<div class="row">
-						<div class="p-3 p-lg-5 border bg-white">
-							<table class="table site-block-order-table mb-5">
-								<thead>
-									<th> Producto </th>
-									<th> Rating </th>
-									<th> Review </th>
-								</thead>
-								<tbody id="tablebody">
-								</tbody>
-							</table>
-						</div>
 					</div>
-				</div>
-			</div>
+					<div class="container">
+					<div id="resultados" class="blog-section">
+    <div class="container">
+        <div class="row">
+            <div class="p-3 p-lg-5 border bg-white">
+                <table class="table site-block-order-table mb-5">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Rating</th>
+                            <th>Review</th>
+                            <th>TimeStamp</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tablebody">
+                        <?php
+						include 'back-end/get_reviews.php';
+                        if ($reviews) {
+                            foreach ($reviews as $review) {
+                                echo '<tr>';
+                                echo '<td>' . htmlspecialchars($review['product_name']) . '</td>';
+                                echo '<td>' . htmlspecialchars($review['rating']) . '</td>';
+                                echo '<td>' . htmlspecialchars($review['review']) . '</td>';
+                                echo '<td>' . htmlspecialchars($review['timestamp']) . '</td>';
+                                echo '</tr>';
+                            }
+                        } else {
+                            echo '<tr><td colspan="4">No reviews yet.</td></tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 
 		</div>
-
-
 		<!-- Start Footer Section -->
 		<footer class="footer-section">
-			<div class="container relative">
+		<div class="container relative">
+		<div class="row g-5 mb-5">
+				<div class="col-lg-4">
+					<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Silverhand Components<span>.</span></a></div>
+					<p class="mb-4">En Silverhan Components, somos apasionados por la tecnología y ofrecemos lo mejor en componentes de PC, consolas de videojuegos y soporte técnico especializado. Trabajamos con marcas líderes para garantizar productos de alta calidad y rendimiento superior.</p>
 
-				<div class="sofa-img">
-					<img src="images/sofa.png" alt="Image" class="img-fluid">
+					<ul class="list-unstyled custom-social">
+						<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
+						<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
+						<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
+						<li><a href="#"><span class="fa fa-brands fa-discord"></span></a></li>
+					</ul>
 				</div>
 
-				<div class="row">
-					<div class="col-lg-8">
-						<div class="subscription-form">
-							<h3 class="d-flex align-items-center"><span class="me-1"><img src="images/envelope-outline.svg" alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
-
-							<form action="#" class="row g-3">
-								<div class="col-auto">
-									<input type="text" class="form-control" placeholder="Enter your name">
-								</div>
-								<div class="col-auto">
-									<input type="email" class="form-control" placeholder="Enter your email">
-								</div>
-								<div class="col-auto">
-									<button class="btn btn-primary">
-										<span class="fa fa-paper-plane"></span>
-									</button>
-								</div>
-							</form>
-
-						</div>
-					</div>
-				</div>
-
-				<div class="row g-5 mb-5">
-					<div class="col-lg-4">
-						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-						<p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
-
-						<ul class="list-unstyled custom-social">
-							<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-instagram"></span></a></li>
-							<li><a href="#"><span class="fa fa-brands fa-linkedin"></span></a></li>
-						</ul>
-					</div>
-
-					<div class="col-lg-8">
-						<div class="row links-wrap">
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">About us</a></li>
-									<li><a href="#">Services</a></li>
-									<li><a href="#">Blog</a></li>
-									<li><a href="#">Contact us</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Support</a></li>
-									<li><a href="#">Knowledge base</a></li>
-									<li><a href="#">Live chat</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Jobs</a></li>
-									<li><a href="#">Our team</a></li>
-									<li><a href="#">Leadership</a></li>
-									<li><a href="#">Privacy Policy</a></li>
-								</ul>
-							</div>
-
-							<div class="col-6 col-sm-6 col-md-3">
-								<ul class="list-unstyled">
-									<li><a href="#">Nordic Chair</a></li>
-									<li><a href="#">Kruzo Aero</a></li>
-									<li><a href="#">Ergonomic Chair</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-
-				</div>
-
-				<div class="border-top copyright">
-					<div class="row pt-4">
-						<div class="col-lg-6">
-							<p class="mb-2 text-center text-lg-start">Copyright &copy;<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a>  Distributed By <a href="https://themewagon.com">ThemeWagon</a> <!-- License information: https://untree.co/license/ -->
-            </p>
-						</div>
-
-						<div class="col-lg-6 text-center text-lg-end">
-							<ul class="list-unstyled d-inline-flex ms-auto">
-								<li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
-								<li><a href="#">Privacy Policy</a></li>
+				<div class="col-lg-8">
+					<div class="row links-wrap">
+						<div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+								<li><a href="#">Acerca de nosotros</a></li>
+								<li><a href="#">Servicios</a></li>
+								<li><a href="#">Noticias</a></li>
+								<li><a href="#">Contáctenos</a></li>
 							</ul>
 						</div>
 
+						<div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+								<li><a href="#">Soporte</a></li>
+								<li><a href="#">Catálogo</a></li>
+								<li><a href="#">Live chat</a></li>
+							</ul>
+						</div>
+
+						<div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+								<li><a href="#">Trabajos</a></li>
+								<li><a href="#">Nuestro equipo</a></li>
+								<li><a href="#">Objetivos</a></li>
+								<li><a href="#">Política de privacidad</a></li>
+							</ul>
+						</div>
+
+						<div class="col-6 col-sm-6 col-md-3">
+							<ul class="list-unstyled">
+								<li><a href="#">Tarjetas gráficas RTX</a></li>
+								<li><a href="#">Procesadores Intel</a></li>
+								<li><a href="#">Ofertas exclusivas</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 
 			</div>
+
+			<div class="border-top copyright">
+				<div class="row pt-4">
+					<div class="col-lg-6">
+						<p class="mb-2 text-center text-lg-start">Copyright &copy;<script>
+								document.write(new Date().getFullYear());
+							</script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> Distributed By <a hreff="https://themewagon.com">ThemeWagon</a> <!-- License information: https://untree.co/license/ -->
+						</p>
+					</div>
+
+					<div class="col-lg-6 text-center text-lg-end">
+						<ul class="list-unstyled d-inline-flex ms-auto">
+							<li class="me-4"><a href="#">Terms &amp; Conditions</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+</div>
 		</footer>
 		<!-- End Footer Section -->	
 
@@ -354,6 +329,7 @@
 		<script src="js/bootstrap.bundle.min.js"></script>
 		<script src="js/tiny-slider.js"></script>
 		<script src="js/custom.js"></script>
+		
 	</body>
 
 </html>
