09c22830788ede366cbd7f7d5509ce321ab35e1c
ItsDiegoTBG <diealcon@espol.edu.ec>
2024-08-25 19:15:33 -0500

    Correciones Front.

--- back-end/get_reviews.php
+++ back-end/get_reviews.php
@@ -1,21 +1,8 @@
 <?php
 require 'firebase.php';
 
-$productName = $_GET['product_name'];
-$reviewsRef = $database->getReference('reviews/'.$productName);
-$reviews = $reviewsRef->getValue();
 
-echo '<h3>Reviews</h3>';
-if ($reviews) {
-    foreach ($reviews as $review) {
-        echo '<div>';
-        echo '<p>ProductName: '.$review['product_name'].'</p>';
-        echo '<p>Rating: '.$review['rating'].'</p>';
-        echo '<p>Review: '.$review['review'].'</p>';
-        echo '<p>Date: '.$review['timestamp'].'</p>';
-        echo '</div>';
-    }
-} else {
-    echo 'No reviews yet.';
-}
+$reviewsRef = $database->getReference('reviews/');
+$reviews = $reviewsRef->getValue();
+$keys = $reviewsRef->getChildKeys();
 ?>
