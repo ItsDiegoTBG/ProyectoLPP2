a39584277c41b356a9b88e2e956716c41fc413b0
ItsDiegoTBG <diealcon@espol.edu.ec>
2024-09-01 17:32:37 -0500

    Extrayendo datos de la base completado

--- back-end/get_reviews.php
+++ back-end/get_reviews.php
@@ -1,8 +1,21 @@
 <?php
 require 'firebase.php';
 
-
+$allItemsArray = array();
 $reviewsRef = $database->getReference('reviews/');
 $reviews = $reviewsRef->getValue();
 $keys = $reviewsRef->getChildKeys();
+
+
+foreach($keys as $key => $productObject){
+    $revR = $database->getReference("reviews/$productObject");
+    $rs = $revR->getValue();
+    foreach($rs as $oKey => $oK){
+        $rR = $database->getReference("reviews/$productObject/$oKey");
+        $r = $rR->getValue();
+        array_push($allItemsArray,$r);
+    }
+   
+}
+
 ?>
